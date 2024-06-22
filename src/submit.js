6b8abb436f472a740e52a5bf37164834512084a5
satyamrkushwaha <satyamkushwahasrk95@gmail.com>
2024-06-22 19:08:25 +0530

    frontend and backedn integrated

--- src/submit.js
+++ src/submit.js
@@ -1,10 +1,32 @@
 // submit.js
+import { useStore } from './store';
 
 export const SubmitButton = () => {
+  const nodes = useStore((state) => state.nodes);
+  const edges = useStore((state) => state.edges);
 
-    return (
-        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
-        </div>
-    );
-}
+  console.log(nodes, edges )
+
+  const handleSubmit = async () => {
+    const response = await fetch('http://localhost:8000/pipelines/parse', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ nodes, edges }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      alert(`Number of Nodes: ${data.num_nodes}, Number of Edges: ${data.num_edges}, Is DAG: ${data.is_dag}`);
+    } else {
+      alert('Failed to parse the pipeline');
+    }
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <button type="button" className='submit-btn' onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
--- src/toolbar.js
+++ src/toolbar.js
@@ -5,7 +5,7 @@ import { DraggableNode } from './draggableNode';
 export const PipelineToolbar = () => {
 
     return (
-        <div style={{ padding: '10px' }}>
+        <div className='pipeline-toolbar-container'>
             <div style={{ marginTop: '20px', display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
                 <DraggableNode type='customInput' label='Input' />
                 <DraggableNode type='llm' label='LLM' />
